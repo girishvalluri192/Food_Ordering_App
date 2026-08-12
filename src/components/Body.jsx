6c226adce81fdfc2704cef2479f65fb00029fb1d
girishvalluri192 <girishvalluri192@email.com>
2026-08-12 11:06:44 +0530

    this is another commit

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -147,14 +147,14 @@ const Body = ()=>{
   const [listOfRestro,setListOfRestro] = useState([]);
 
   // useEffect Hook
-  useEffect(()=>{fetchData()},[]);
+//   useEffect(()=>{fetchData()},[]);
 
-  const fetchData = async ()=>{
-    const data = await fetch("https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=17.362825302557773&lng=78.55158694088459&carousel=true&third_party_vendor=1");
-    const json = await data.json();
-    console.log(json);
-    setListOfRestro(json.data.cards[2].card.card);
- } 
+//   const fetchData = async ()=>{
+//     const data = await fetch("https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=17.362825302557773&lng=78.55158694088459&carousel=true&third_party_vendor=1");
+//     const json = await data.json();
+//     console.log(json);
+//     setListOfRestro(json.data.cards[2].card.card);
+//  } 
 
     return (
         <div className="body">
@@ -174,7 +174,7 @@ const Body = ()=>{
                  {listOfRestro.map((restaurant) => (
                     <RestroCard
                      key={restaurant.card.info.id}
-                     resData={restaurant.card.info}
+                     {...restaurant}
                      />
                     ))}
 
