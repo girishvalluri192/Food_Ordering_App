b490dece47f72a4cd76d8778706f3a97d3bb624b
girishvalluri192 <girishvalluri192@email.com>
2026-09-10 16:47:23 +0530

    todays about us page small commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,8 @@ const About = ()=>{
         <div>
             <h1>About Us</h1>
             <h3>This is Namaste React web series </h3>
+
+            <h5>This is just an example page for About Us </h5>
         </div>
     )
 };
