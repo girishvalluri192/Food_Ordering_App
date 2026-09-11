adf467bacd177139c9d83544f81216a3bf9b07b0
Girish Valluri <girishvalluri192@gmail.com>
2026-09-11 15:26:26 +0530

    This is commit to check the green dot

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,8 @@ const About = ()=>{
             <h3>This is Namaste React web series </h3>
 
             <h5>This is just an example page for About Us </h5>
-            <h5>This is the line added after git update </h5>
-             <h5>This is the line added after git verification update </h5>
+            <h5>This heading line has been made to check the Git hub push operation result in profile for a  green dot</h5>
+
         </div>
     )
 };
