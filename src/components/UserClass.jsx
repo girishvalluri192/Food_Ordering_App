365d874b33b77f7af1a2cc522bc5013a9e043b06
Girish Valluri <girishvalluri192@gmail.com>
2026-09-19 11:09:41 +0530

    This is todays second commit

---
+++ src/components/UserClass.jsx
@@ -0,0 +1,11 @@
+class UserClass extends React.Component{
+    render(){
+        return (
+            <div>
+                <h3>This is a class based component</h3>
+                <h4>Name : Akshay</h4>
+                <h5>Location : Deharadun</h5>
+            </div>
+        )
+    }
+}
